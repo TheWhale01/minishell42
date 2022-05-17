5ca3a022b442a4eeb046e8839469a315136c1c41
Jules ROSSETTI <jrossett@e1r3p15.clusters.42paris.fr>
2022-05-17 04:41:47 +0200

    fixed main

--- src/main.c
+++ src/main.c
@@ -3,20 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hubretec <hubretec@student.42.fr >         +#+  +:+       +#+        */
+/*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 21:33:00 by hubretec          #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2022/05/11 16:24:51 by jrossett         ###   ########.fr       */
-=======
-/*   Updated: 2022/05/13 11:56:48 by hubretec         ###   ########.fr       */
->>>>>>> 6785dee8e7b5d753dfee3092bbcd123b3a99010f
-=======
-/*   Updated: 2022/05/16 23:00:25 by hubretec         ###   ########.fr       */
->>>>>>> 6be0243473838ddc6b06a7216b1d62eac8cafe42
+/*   Updated: 2022/05/17 04:41:17 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "minishell.h"
 
@@ -33,15 +26,6 @@ int	main(int ac, char **av, char **envp)
 	t_data	data;
 	t_list	*lst;
 
-<<<<<<< HEAD
-	(void) envp;
-	(void) ac;
-	(void) av;
-	list = create_list(envp);
-	//ft_env(&list);
-	//free_list(list);
-	ft_export(list, "export e=edwed dewed  dewde dewded   ");
-=======
 	(void)ac;
 	(void)av;
 	init(&data);
@@ -61,7 +45,6 @@ int	main(int ac, char **av, char **envp)
 		print_tokens(data.tokens, 1);
 		free(data.line);
 	}
->>>>>>> 6785dee8e7b5d753dfee3092bbcd123b3a99010f
 	return (0);
 }
 
